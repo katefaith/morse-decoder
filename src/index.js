9b392109c038f1e113afb1cc033d73c0f4c42552
Kate <chelsea-92@mail.ru>
2020-03-11 01:19:42 +0300

    feat: implement decode(expr) function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    		// преобразуем строку в массив из зашифрованных букв
+            let exprArr = [];
+            let exprArrLength = expr.length / 10
+            for (let i = 0; i < exprArrLength; i++) {
+                exprArr[i] = expr.slice(10 * i, 10 * (i + 1));
+            }
+            
+            // обрезаем у элементов массива нули слева, преобразуем звездочки в пробелы
+            exprArr = exprArr.map(item => {
+                if (item === '**********') {
+                  return ' ';
+              } else {
+                for (let i = 0; i < item.length; i++) {
+                  if (item[i] === '1') {
+                    let newItem = item.slice(i);
+                    return newItem;
+                  }
+                }
+              }
+            });
+            
+            // преобразуем в точки и тире
+            exprArr = exprArr.map(item => {
+                let newItem = '';
+              if (item === ' ') {
+                  newItem = item;
+              } else {
+                  for (let i = 1; i < item.length; i+=2) {
+                    newItem += (item[i] === '0') ? '.' : '-';
+                }
+              }
+              return newItem;
+            });
+            
+            // преобразуем в буквы
+            exprArr = exprArr.map(item => {
+                if (item in MORSE_TABLE) {
+                  return MORSE_TABLE[item];
+              } else {
+                  return ' ';
+              }
+            });
+            
+            return exprArr.join('');
 }
 
 module.exports = {
