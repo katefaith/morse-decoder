a7660c1a5495809f88e59009e1190785ed8f0bb0
Kate <chelsea-92@mail.ru>
2020-03-11 01:32:33 +0300

    refactor: improve the algorithm

--- src/index.js
+++ src/index.js
@@ -1,87 +1,63 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    		// преобразуем строку в массив из зашифрованных букв
-            let exprArr = [];
-            let exprArrLength = expr.length / 10
-            for (let i = 0; i < exprArrLength; i++) {
-                exprArr[i] = expr.slice(10 * i, 10 * (i + 1));
+    // преобразуем строку в массив из зашифрованных букв
+    let exprArr = [];
+    for (let i = 0; i < expr.length / 10; i++) {
+        exprArr[i] = expr.slice(10 * i, 10 * (i + 1));
+    }
+
+    return exprArr.map(item => {
+        let newItem = '';
+        // преобразуем элементы массива в точки и тире
+        for (let i = 0; i < item.length; i += 2) {
+            if (item.substr(i, 2) === '10') {
+                newItem += '.';
             }
-            
-            // обрезаем у элементов массива нули слева, преобразуем звездочки в пробелы
-            exprArr = exprArr.map(item => {
-                if (item === '**********') {
-                  return ' ';
-              } else {
-                for (let i = 0; i < item.length; i++) {
-                  if (item[i] === '1') {
-                    let newItem = item.slice(i);
-                    return newItem;
-                  }
-                }
-              }
-            });
-            
-            // преобразуем в точки и тире
-            exprArr = exprArr.map(item => {
-                let newItem = '';
-              if (item === ' ') {
-                  newItem = item;
-              } else {
-                  for (let i = 1; i < item.length; i+=2) {
-                    newItem += (item[i] === '0') ? '.' : '-';
-                }
-              }
-              return newItem;
-            });
-            
-            // преобразуем в буквы
-            exprArr = exprArr.map(item => {
-                if (item in MORSE_TABLE) {
-                  return MORSE_TABLE[item];
-              } else {
-                  return ' ';
-              }
-            });
-            
-            return exprArr.join('');
+            if (item.substr(i, 2) === '11') {
+                newItem += '-';
+            }
+        }
+        // преобразуем в буквы
+        return (newItem in MORSE_TABLE) ? MORSE_TABLE[newItem] : ' ';
+    }).join(''); // возвращаем строку
 }
 
 module.exports = {
